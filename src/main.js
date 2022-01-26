4bee2283cd4b111a8a9098d13488fc190da0fc17
J-VOL <57736432+J-VOL@users.noreply.github.com>
2022-01-26 14:31:35 -0500

    login testing xhr

--- src/main.js
+++ src/main.js
@@ -1,3 +1,112 @@
+const urlBase = 'https://cop4331.acobble.io/';
+const extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+let isPasswordMatch = false;
+let isPasswordValid = false;
+const passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}/;
+
+const regForm = document.getElementById("regForm");
+const loginForm = document.getElementById("loginForm");
+
+const badLoginMsg = "Login not recognized";
+const badUserRegMsg = "Username taken"
+const badPassMsg = "Password Requirements:<ul><li>Must contain a number, a special character, an uppercase letter, and a lower case letter</li><li>Is at least 8 characters long</li></ul>";
+const passMismatch = "Passwords do not match";
+
+// loginForm.addEventListener('submit', function(event) {
+//
+//     let userInput = document.getElementById("loginUser");
+//     let passInput = document.getElementById("loginPass");
+//
+//     if (loginForm.checkValidity() === false) {//!userInput.checkValidity() || !passInput.checkValidity()
+//         event.preventDefault();
+//         event.stopPropagation();
+//         // return;
+//     }
+//     if (!doLogin(userInput, passInput)){
+//         event.preventDefault();
+//         event.stopPropagation();
+//
+//         userInput.setCustomValidity(badLoginMsg);
+//         document.getElementById("userValMsg").innerHTML = badLoginMsg;
+//
+//         passInput.setCustomValidity(badLoginMsg);
+//         document.getElementById("passValMsg").innerHTML = badLoginMsg;
+//         // return;
+//     }
+//
+//     loginForm.classList.add('was-validated');
+// }, false);
+
+// regForm.addEventListener('input', function(event) {
+//     const formData = new FormData(regForm);
+//     const password = formData.get("password").toString();
+//     const repeatPassword = formData.get("repeatPassword");
+//
+//     // Validate the pattern again.
+//     isPasswordValid = passwordPattern.test(password);
+//     isPasswordMatch = repeatPassword !== '' && password === repeatPassword;
+// }, false);
+
+regForm.addEventListener('submit', function(event) {
+
+    let userInput = document.getElementById("regUser");
+    let passInput = document.getElementById("regPass");
+    let repeatInput = document.getElementById("regRepeatPass");
+
+    if (!regForm.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+        // return;
+    }
+
+    if (passInput.validity.patternMismatch){
+        passInput.setCustomValidity(badPassMsg);
+        document.getElementById("regPassValMsg").innerHTML = badPassMsg;
+        event.preventDefault();
+        event.stopPropagation();
+        // return;
+    }
+
+    const formData = new FormData(regForm);
+    const password = formData.get("password").toString();
+    const repeatPassword = formData.get("repeatPassword");
+    isPasswordMatch = password === repeatPassword;
+
+    if (isPasswordMatch !== true){
+        passInput.setCustomValidity(passMismatch);
+        document.getElementById("regPassValMsg").innerHTML = passMismatch;
+
+        repeatInput.setCustomValidity(passMismatch);
+        document.getElementById("regRepeatValMsg").innerHTML = passMismatch;
+        event.preventDefault();
+        event.stopPropagation();
+        // return;
+    }
+
+    /*
+    if(!doRegister(userInput, passInput)){
+        event.preventDefault();
+        event.stopPropagation();
+
+        userInput.setCustomValidity(badUserRegMsg);
+        document.getElementById("regUserValMsg").innerHTML = badUserRegMsg;
+        return;
+    }*/
+
+    regForm.classList.add('was-validated');
+}, false);
+
+
+
+function doRegister(login, password){
+
+}
+
 (function () {
     'use strict'
 
@@ -22,13 +131,65 @@ function makeEventListeners (){
     makeLoginEventListeners()
 }
 function getLoginInfo(){
-    let user = document.getElementById("loginUser").value
-    let pass = document.getElementById("loginPass").value
-    return {user,pass}
+    let login = document.getElementById("loginUser").value
+    let password = document.getElementById("loginPass").value
+    return {login,password}
 }
+
 function userLogin(){
     let loginInfo= getLoginInfo()
     console.log(loginInfo.pass)
+
+    // function doLogin(login, password){
+        userId = 0;
+        firstName = "";
+        lastName = "";
+
+        //	var hash = md5( password );
+
+        let jsonPayload = JSON.stringify(loginInfo)
+
+
+        let url = urlBase + '/Login.' + extension;
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+            xhr.onreadystatechange = function()
+            {
+                if (this.readyState === 4 && this.status === 200)
+                {
+                    let jsonObject = JSON.parse( xhr.responseText );
+                    userId = jsonObject.id;
+
+                    if( userId < 1 )
+                    {
+                        // document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+                        return false;
+                    }
+
+                    // <<<<<<< HEAD
+                    // firstName = jsonObject.firstName;
+                    // lastName = jsonObject.lastName;
+
+                    // saveCookie();
+
+                    // window.location.href = "color.html";
+                }
+            };
+            xhr.send(jsonPayload);
+        }
+        catch(err)
+        {
+            document.getElementById("passValMsg").innerHTML = badLoginMsg;
+            // document.getElementById("passValMsg").innerHTML = err.message;
+            // console.log("Lo")
+            return false;
+        }
+
+    // }
 
 }
 function makeLoginEventListeners(){
@@ -36,3 +197,4 @@ function makeLoginEventListeners(){
     loginButton.addEventListener("click",userLogin)
 }
 makeEventListeners()
+
