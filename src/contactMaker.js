66c651cb49571415aeae412530fc9be6e149d28c
Santiago  Gutierrez <santigut@posteo.net>
2022-02-02 11:54:05 -0500

    more functionality for contact div maker

--- src/contactMaker.js
+++ src/contactMaker.js
@@ -2,9 +2,17 @@ function makeContactDiv(contact){
   let contactDiv = document.createElement("div");
 }
 function fillContactDiv(contact,contactDiv){
+  // setting contact attributes
   contact.setAttribute("class","contact");
+  contact.setAttribute("id",contact.ID);
+  // adding the name part
   let contactName = document.createElement("div");
   contactName.setAttribute("class","contactNameText");
   contactName.innerHTML = contact.firstName +" "+contact.lastName;
-  contactName.setAttribute("contactID","")
+  // add the button
+  let extendButton = document.createElement("button");
+  extendButton.setAttribute("class","contactExtendButton");
+  extendButton.setAttribute("contactID",contact.ID);
+  // here we would add the event listener
+  contact.appendChild(contactName);
 }
