f997d89d5030701a8a04521c75a0259281e89e16
treenamu <82667353+treenamu@users.noreply.github.com>
2022-02-02 16:04:51 -0500

    Add files via upload

---
+++ src/API/SearchContacts.php
@@ -0,0 +1,74 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "MAINUSER", "COP4331Project!", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE
+		(FirstName LIKE ? OR LastName LIKE ? OR PhoneNumber Like ? OR EmailAddress LIKE ?) AND UserID=? LIMIT 20");
+		$contactSearch = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ssssi", $contactSearch, $contactSearch, $contactSearch, $contactSearch, $inData["userId"]);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["Name"] . '"';
+		}
+
+		// No search results
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+
+
+	// Functions
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
