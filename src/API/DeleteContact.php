fcdd0d97805b7affa47305b53cf31fd5edc25f8e
treenamu <82667353+treenamu@users.noreply.github.com>
2022-02-04 16:52:29 -0500

    Add files via upload

---
+++ src/API/DeleteContact.php
@@ -0,0 +1,50 @@
+<?php
+	$inData = getRequestInfo();
+
+	$contactId = $inData["contactId"];
+
+	$conn = new mysqli("localhost", "MAINUSER", "COP4331Project!", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		// Search to see if an account
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE ID=?");
+		$stmt->bind_param("i", $contactId);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		// If search result comes up, delete account
+		if( $row = $result->fetch_assoc() )
+		{
+			$stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=?");
+			$stmt->bind_param("i", $contactId);
+			$stmt->execute();
+		}
+		else
+		{
+			returnWithError( "No Record Found" );
+		}
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
