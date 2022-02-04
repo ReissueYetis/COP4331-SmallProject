ac9d6ab57ac5d4016299dccb69948829789426fd
treenamu <82667353+treenamu@users.noreply.github.com>
2022-02-04 18:07:14 -0500

    Add files via upload

--- src/API/DeleteContact.php
+++ src/API/DeleteContact.php
@@ -2,6 +2,7 @@
 	$inData = getRequestInfo();
 
 	$contactId = $inData["contactId"];
+	$deletedContact = "";
 
 	$conn = new mysqli("localhost", "MAINUSER", "COP4331Project!", "COP4331");
 	if ($conn->connect_error)
@@ -20,7 +21,17 @@
 		{
 			$stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=?");
 			$stmt->bind_param("i", $contactId);
+
+			$deletedContact .= '"ID": ' . $row["ID"] . ',';
+			$deletedContact .= '"FirstName": "' . $row["FirstName"] . '",';
+			$deletedContact .= '"LastName": "' . $row["LastName"] . '",';
+			$deletedContact .= '"PhoneNumber": "' . $row["PhoneNumber"] . '",';
+			$deletedContact .= '"EmailAddress": "' . $row["EmailAddress"] . '",';
+			$deletedContact .= '"DateCreated": "' . $row["DateCreated"] . '",';
+			$deletedContact .= '"UserID": ' . $row["UserID"];
+
 			$stmt->execute();
+			returnWithInfo( $deletedContact );
 		}
 		else
 		{
