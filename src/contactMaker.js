36575d60744f15e2e371b6d731b205f9c01578a4
Santiago Gutierrez <santigut@posteo.net>
2022-02-06 11:19:54 -0500

    starting the process for search div gen

--- src/contactMaker.js
+++ src/contactMaker.js
@@ -1,6 +1,7 @@
 function makeContactDiv(contact){
   let contactDiv = document.createElement("div");
 }
+
 function fillContactDiv(contact,contactDiv){
   // setting contact attributes
   contact.setAttribute("class","contact");
@@ -16,6 +17,7 @@ function fillContactDiv(contact,contactDiv){
   // here we would add the event listener
   contact.appendChild(contactName);
 }
+
 function getContactInfo(contact){
   let email = contact.email;
   let phoneNum = contact.phoneNum;
@@ -23,3 +25,26 @@ function getContactInfo(contact){
   contactInfoDiv.innerHTML = email + "  "+ phoneNum;
   return contactInfoDiv;
 }
+
+function myCallback(status, data) {
+    console.log(data);
+}
+
+function postJSONSearch(url, json_data) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.responseType = "json";
+    xhr.send(JSON.stringify(json_data));
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            myCallback(null, xhr.response);
+        } else {
+            myCallback(status, xhr.response);
+        }
+    }
+}
+
+let demoRequest = {"userId":"55","search",""};
+postJSONSearch("https://cop4331.acobble.io/Search.php",demoRequest);
--- src/contacts.js
+++ src/contacts.js
@@ -150,3 +150,53 @@ $(function() {
     })
 })
 // $.validator.setDefaults({});
+function makeContactDiv(contact){
+  let contactDiv = document.createElement("div");
+}
+
+function fillContactDiv(contact,contactDiv){
+  // setting contact attributes
+  contact.setAttribute("class","contact");
+  contact.setAttribute("id",contact.ID);
+  // adding the name part
+  let contactName = document.createElement("div");
+  contactName.setAttribute("class","contactNameText");
+  contactName.innerHTML = contact.firstName +" "+contact.lastName;
+  // add the button
+  let extendButton = document.createElement("button");
+  extendButton.setAttribute("class","contactExtendButton");
+  extendButton.setAttribute("contactID",contact.ID);
+  // here we would add the event listener
+  contact.appendChild(contactName);
+}
+
+function getContactInfo(contact){
+  let email = contact.email;
+  let phoneNum = contact.phoneNum;
+  let contactInfoDiv = document.createElement("div");
+  contactInfoDiv.innerHTML = email + "  "+ phoneNum;
+  return contactInfoDiv;
+}
+
+function myCallback(status, data) {
+    console.log(data);
+}
+
+function postJSONSearch(url, json_data) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.responseType = "json";
+    xhr.send(JSON.stringify(json_data));
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            myCallback(null, xhr.response);
+        } else {
+            myCallback(status, xhr.response);
+        }
+    }
+}
+
+let demoRequest = {"userId":"55","search":""};
+postJSONSearch("https://cop4331.acobble.io/Search.php",demoRequest);
